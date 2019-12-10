39a8a644de7398e145bcfd5c80e535708f3aada8
Jeewan Thapa Magar <jeewanmgr29@gmail.com>
2019-12-10 14:06:55 -0500

    Add files via upload

---
+++ DoubleLinkedList/dList.java
@@ -0,0 +1,19 @@
+
+public class dList<T> {
+	private Node head;
+	private Node tail;
+	private int size; 
+	//default constructor
+	
+	//getsize();
+	
+	//design insert(T e) that will insert in the front
+	
+	//insertBack(T e) that will insert in the tail
+	
+	// T remove()  that will remove the tail and return the data
+	
+	//T removeFront() that will remove the Node at the head and return the data 
+	
+	//T At(int index) returns the data at element at index i 0 is front size-1 is tail. 
+}
